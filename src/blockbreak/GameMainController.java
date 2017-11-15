d004abb056c92be43240a35cdf48c931a6e5e844
ririron <townageririon@gmail.com>
2017-11-15 10:15:15 +0900

    paddle
    相手に座標送るのはキー押した時だけ、
    常に送ってると重い

--- src/blockbreak/GameMainController.java
+++ src/blockbreak/GameMainController.java
@@ -22,155 +22,178 @@ import javafx.scene.text.Text;
  * @author PCUser
  */
 public class GameMainController implements Initializable {
+@FXML
+private Text myName;
 
-    @FXML
-    private Text myName;
+@FXML
+private Text opponentName;
 
-    @FXML
-    private Text opponentName;
+@FXML
+private Pane root;
 
-    @FXML
-    private Pane root;
+@FXML
+private Rectangle EnemyPaddle;
 
-    @FXML
-    private Rectangle upperpaddle;
+@FXML
+private Rectangle MyPaddle;
 
-    @FXML
-    private Rectangle underpaddle;
+/**
+ * instanvce(singleton)
+ */
+private static final GameMainController INSTANCE;
 
-    /**
-     * instanvce(singleton)
-     */
-    private static final GameMainController INSTANCE;
+/**
+ * Scene(singleton)
+ */
+private static final Scene SCENE;
 
-    /**
-     * Scene(singleton)
-     */
-    private static final Scene SCENE;
+/**
+ * for sending to Server
+ */
+Circle ball;
+PrintWriter myOut;
 
-    /**
-     * for sending to Server
-     */
-    Circle ball;
-    PrintWriter myOut;
+static {
+  FXMLLoader fxmlLoader = new FXMLLoader(BlockBreak.class.getResource("GameMain.fxml"));
+  try {
+    fxmlLoader.load();
+  }
+  catch (IOException e) {
+    e.getStackTrace();
+  }
+  Parent parent = fxmlLoader.getRoot();
+  parent.requestFocus();
 
-    static {
-        FXMLLoader fxmlLoader = new FXMLLoader(BlockBreak.class.getResource("GameMain.fxml"));
-        try {
-            fxmlLoader.load();
-        } catch (IOException e) {
-            e.getStackTrace();
+  Scene s = new Scene(parent);
+  s.getRoot().requestFocus();
+
+  s.setFill(Color.TRANSPARENT);
+  SCENE = s;
+  INSTANCE = fxmlLoader.getController();
+}
+
+public GameMainController() {
+  ball = new Circle(5.0);
+  MesgRecvThread mrt = new MesgRecvThread(BlockBreak.getSocket(), BlockBreak.getUserName());
+  mrt.start();
+}
+
+public float MoveLeft(Rectangle Paddle, float mySpeed) {
+  mySpeed -= 1.42;
+  return mySpeed;
+}
+
+public float MoveRight(Rectangle Paddle, float mySpeed) {
+  mySpeed += 1.42;
+  return mySpeed;
+}
+
+public float MovePaddle(Rectangle Paddle, float mySpeed) {
+  if (mySpeed == 0.0) mySpeed += 0.0;
+  if (mySpeed < 0.0) {
+    Paddle.setX(Math.max(Paddle.getX() + mySpeed, 150 * -1 + Paddle.getWidth() / 2));
+    mySpeed *= 0.96;
+  }
+  if (mySpeed > 0.0) {
+    Paddle.setX(Math.min(Paddle.getX() + mySpeed, 150 - Paddle.getWidth() / 2));
+    mySpeed *= 0.96;
+  }
+  return mySpeed;
+}
+
+public class MesgRecvThread extends Thread {
+Socket socket;
+String myName;
+float mySpeed = 0;
+
+public MesgRecvThread(Socket s, String n) {
+  socket = s;
+  myName = n;
+}
+
+@Override
+public void run() {
+  try {
+    InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+    BufferedReader br = new BufferedReader(isr);
+
+    myOut = new PrintWriter(socket.getOutputStream(), true);
+    myOut.println(myName);
+    while (true) {
+      String inputLine = br.readLine();
+      if (inputLine != null) {
+        System.out.println("ReceiveMessage:" + inputLine);
+        String[] inputTokens = inputLine.split(",", -1);
+        String cmd = inputTokens[0];
+
+        System.out.println("First word:" + cmd);
+
+
+
+        if (cmd.equals("Paddle")) {
+          String cmd2 = inputTokens[1];
+          String cmd3 = inputTokens[2];
+          if (cmd3.equals(myName)) {
+            if (cmd2.equals("LEFT")) mySpeed = MoveLeft(MyPaddle, mySpeed);
+            if (cmd2.equals("RIGHT")) mySpeed =  MoveRight(MyPaddle, mySpeed);
+          }
+          String SendMesg = new String("Paddle," + myName + "," + MyPaddle.getX());
+          myOut.println(SendMesg);
         }
-        Parent parent = fxmlLoader.getRoot();
-        parent.requestFocus();
-
-	Scene s = new Scene(parent);
-        s.getRoot().requestFocus();
-
-        s.setFill(Color.TRANSPARENT);
-        SCENE = s;
-        INSTANCE = fxmlLoader.getController();
-    }
-
-    public GameMainController() {
-        ball = new Circle(5.0);
-        MesgRecvThread mrt = new MesgRecvThread(BlockBreak.getSocket(), BlockBreak.getUserName());
-        mrt.start();
-    }
-    public void PaddleMoveLeft(String SendNum) {
-      if(SendNum.equals("0")) underpaddle.setX(Math.max(underpaddle.getX()-20,150*-1+underpaddle.getWidth()/2));
-      if(SendNum.equals("1")) upperpaddle.setX(Math.max(upperpaddle.getX()-20,150*-1+upperpaddle.getWidth()/2));
-    }
-    public void PaddleMoveRight(String SendNum){
-      if(SendNum.equals("0")) underpaddle.setX(Math.min(underpaddle.getX()+20,150-underpaddle.getWidth()/2));
-      if(SendNum.equals("1")) upperpaddle.setX(Math.min(upperpaddle.getX()+20,150-upperpaddle.getWidth()/2));
-    }
-
-    public class MesgRecvThread extends Thread {
-
-        Socket socket;
-        String myName;
-
-        public MesgRecvThread(Socket s, String n) {
-            socket = s;
-            myName = n;
+        if (cmd.equals("Ball")) {
+          ball.setCenterX(Integer.parseInt(inputTokens[2]));
+          ball.setCenterY(Integer.parseInt(inputTokens[3]));
         }
-
-        @Override
-        public void run() {
-            try {
-                InputStreamReader isr = new InputStreamReader(socket.getInputStream());
-                BufferedReader br = new BufferedReader(isr);
-
-                myOut = new PrintWriter(socket.getOutputStream(), true);
-                myOut.println(myName);
-                while(true) {
-                    String inputLine = br.readLine();
-                    if(inputLine != null) {
-
-                        System.out.println(inputLine);
-                        String[] inputTokens = inputLine.split(",",-1);
-                        String cmd = inputTokens[0];
-                        System.out.println(cmd);
-
-
-                    System.out.println(inputTokens.length);
-                      if(cmd.equals("Paddle")){
-                          String cmd2 = inputTokens[1];
-                          String cmd3 = inputTokens[2];
-
-                          if(cmd2.equals("LEFT")){
-                            PaddleMoveLeft(cmd3);
-                        }
-                          if(cmd2.equals("RIGHT")){
-                            PaddleMoveRight(cmd3);
-                        }
-                      }
-                      if(cmd.equals("Ball")){
-                            ball.setCenterX(Integer.parseInt(inputTokens[2]));
-			                      ball.setCenterY(Integer.parseInt(inputTokens[3]));
-                        }
-                    }else{
-                        break;
-                    }
-                }
-		socket.close();
-            } catch (IOException e) {
-                System.err.println("error occured: " + e);
-            }
+        if (cmd.equals("EnemyPaddle")) {
+          if (inputTokens[1].equals(myName)) {
+          }
+          else {
+            EnemyPaddle.setX(Float.valueOf(inputTokens[2]));
+          }
         }
+      }
+      else {
+        break;
+      }
+      mySpeed = MovePaddle(MyPaddle, mySpeed);
+      
     }
-
-    /**
-     * return instance(singleton)
-     * @return INSTANCE
-     */
-    public static GameMainController getInstance() {
-        return INSTANCE;
-    }
-
-    public void show() {
-        BlockBreak.getPresentStage().setScene(SCENE);
-    }
-
-    @FXML
-    private void handleKeyPressed(KeyEvent event) {
-        System.out.println("keypressed");
-        System.out.println(event.getCode());
-	myOut.println(event.getCode());
-	myOut.flush();
-    }
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        myName.setText(BlockBreak.getUserName());
-	root.getChildren().add(ball);
-    }
+    socket.close();
+  }
+  catch (IOException e) {
+    System.err.println("error occured: " + e);
+  }
+}
+}
 
 
+/**
+ * return instance(singleton)
+ * @return INSTANCE
+ */
+public static GameMainController getInstance() {
+  return INSTANCE;
+}
 
+public void show() {
+  BlockBreak.getPresentStage().setScene(SCENE);
+}
+
+@FXML
+private void handleKeyPressed(KeyEvent event) {
+  System.out.println("keypressed");
+  System.out.println(event.getCode());
+  myOut.println(event.getCode());
+  myOut.flush();
+}
+
+/**
+ * Initializes the controller class.
+ */
+@Override
+public void initialize(URL url, ResourceBundle rb) {
+  // TODO
+  myName.setText(BlockBreak.getUserName());
+  root.getChildren().add(ball);
+}
 }
--- src/blockbreak/Server.java
+++ src/blockbreak/Server.java
@@ -22,151 +22,163 @@ import java.util.ArrayList;
  * Server Side
  */
 class ClientProcThread extends Thread {
-    private int number;
-    private Socket incoming;
-    private InputStreamReader myIsr;
-    private BufferedReader myIn;
-    private PrintWriter myOut;
-    private String myName;
+private int number;
+private Socket incoming;
+private InputStreamReader myIsr;
+private BufferedReader myIn;
+private PrintWriter myOut;
+private String myName;
 
-    public  ClientProcThread(int n, Socket i, InputStreamReader isr,
-                                BufferedReader in, PrintWriter out) {
-        number = n;
-        incoming = i;
-        myIsr = isr;
-        myIn = in;
-        myOut = out;
-
-    }
-
-    @Override
-    public void run() {
-        try {
-            myOut.println("Hello, client No." + number + "!");
-        
-
-            myName = myIn.readLine();
-
-            String keycode = " "; //クライアントに送信するキーのコード
-
-            // watching input to socket
-            while(true) {
-                String str = myIn.readLine();
-
-                System.out.println("Receive from client No." + number +
-                                   "(" + myName + "), Messages: " + str);
-                if(str != null) {
-                    Server.SendAll(str);
-                    keycode =str;
-                  }
-              String paddle = new String("Paddle,"+keycode+","+number);
-              Server.SendAll(paddle);
-            }
-        } catch (IOException e) {
-
-        }
-    }
+public ClientProcThread(int n, Socket i, InputStreamReader isr,
+                        BufferedReader in, PrintWriter out) {
+  number = n;
+  incoming = i;
+  myIsr = isr;
+  myIn = in;
+  myOut = out;
 }
+
+@Override
+public void run() {
+  try {
+    myOut.println("Hello, client No." + number + "!");
+
+
+    myName = myIn.readLine();
+
+    String keycode = " ";         // クライアントに送信するキーのコード
+
+    // watching input to socket
+    while (true) {
+      String str = myIn.readLine();
+      String[] input = str.split(",", -1);
+      System.out.println("Receive from client No." + number +
+                         "(" + myName + "), Messages: " + str);
+      if (str != null) {
+        if (input[0].equals("Paddle")) {
+          String enemy = new String("EnemyPaddle," + myName + "," + input[2]);
+          Server.SendAll(enemy);
+        }
+        else {
+          Server.SendAll(str);
+          keycode = str;
+          String paddle = new String("Paddle," + keycode + "," + myName);
+          Server.SendAll(paddle);
+        }
+      }
+
+      try {
+        Thread.sleep(1);
+      }
+      catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  catch (IOException e) {
+  }
+}
+}
+
 
 class BallMoveThread extends Thread {
-    private int id;
-    private int x;
-    private int y;
-    private int xVec = 1;
-    private int yVec = 1;
+private int id;
+private int x;
+private int y;
+private int xVec = 1;
+private int yVec = 1;
 
-    public BallMoveThread(int num,int x, int y) {
-	id = num;
-	x = 450;
-	y = 450;
-    }
-
-    @Override
-    public void run() {
-	while(true){
-	    move();
-	    String str = new String("Ball," + id + "," + x + "," + y + ",");
-	    Server.SendAll(str);
-	    try{
-		Thread.sleep(16);
-	    } catch (InterruptedException e) {
-		e.printStackTrace();
-	    }
-	}
-    }
-
-    private void move() {
-        if(x < 5){
-	    xVec = 2;
-	}else if(x > 295){
-	    xVec = -2;
-	}
-
-	if(y < 5){
-	    yVec = 3;
-	}else if(y > 595){
-	    yVec = -3;
-	}
-
-	x += xVec;
-	y += yVec;
-    }
+public BallMoveThread(int num, int x, int y) {
+  id = num;
+  x = 450;
+  y = 450;
 }
 
+@Override
+public void run() {
+  while (true) {
+    move();
+    String str = new String("Ball," + id + "," + x + "," + y + ",");
+    Server.SendAll(str);
+    try {
+      Thread.sleep(16);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+private void move() {
+  if (x < 5)
+    xVec = 2;
+  else if (x > 295)
+    xVec = -2;
+
+  if (y < 5)
+    yVec = 3;
+  else if (y > 595)
+    yVec = -3;
+
+  x += xVec;
+  y += yVec;
+}
+}
+
+
 public class Server {
+private static int maxConnection = 2;
+private static ArrayList <Socket> incoming;
+private static ArrayList <InputStreamReader> isr;
+private static ArrayList <BufferedReader> in;
+private static ArrayList <PrintWriter> out;
+private static ArrayList <ClientProcThread> myClientProcThread;
+private static ArrayList <BallMoveThread> myBallMoveThread;
 
-    private static int maxConnection = 2;
-    private static ArrayList<Socket> incoming;
-    private static ArrayList<InputStreamReader> isr;
-    private static ArrayList<BufferedReader> in;
-    private static ArrayList<PrintWriter> out;
-    private static ArrayList<ClientProcThread> myClientProcThread;
-    private static ArrayList<BallMoveThread> myBallMoveThread;
+public static void SendAll(String str) {
+  for (int i = 0; i < incoming.size(); i++) {
+    out.get(i).println(str);
+    out.get(i).flush();
+    System.out.println("Send messages to client No." + i);
+  }
+}
 
-    public static void SendAll(String str){
-        for(int i=0; i<incoming.size(); i++){
-            out.get(i).println(str);
-            out.get(i).flush();
-            System.out.println("Send messages to client No." + i);
-        }
+public static void main(String[] args) {
+  incoming = new ArrayList <Socket>();
+  isr = new ArrayList <InputStreamReader>();
+  in = new ArrayList <BufferedReader>();
+  out = new ArrayList <PrintWriter>();
+  myClientProcThread = new ArrayList <ClientProcThread>();
+  myBallMoveThread = new ArrayList <BallMoveThread>();
+
+  int n;
+  int numBall = 0;
+
+  try {
+    System.out.println("The server has launched!");
+    ServerSocket server = new ServerSocket(10027);
+    while (true) {
+      n = incoming.size();
+      incoming.add(server.accept());
+      System.out.println("Accept client No." + n);
+
+      isr.add(new InputStreamReader(incoming.get(n).getInputStream()));
+      in.add(new BufferedReader(isr.get(n)));
+      out.add(new PrintWriter(incoming.get(n).getOutputStream(), true));
+
+      myClientProcThread.add(
+        new ClientProcThread(n, incoming.get(n), isr.get(n), in.get(n), out.get(n)));
+      myClientProcThread.get(n).start();           // start thread
+
+      if (myClientProcThread.size() == 2) {
+        numBall = myBallMoveThread.size();
+        myBallMoveThread.add(new BallMoveThread(numBall, 400, 400));
+        myBallMoveThread.get(numBall).start();
+      }
     }
-
-    public static void main(String[] args) {
-
-        incoming = new ArrayList<Socket>();
-        isr = new ArrayList<InputStreamReader>();
-        in = new ArrayList<BufferedReader>();
-        out = new ArrayList<PrintWriter>();
-        myClientProcThread = new ArrayList<ClientProcThread>();
-	myBallMoveThread = new ArrayList<BallMoveThread>();
-
-        int n;
-	int numBall=0;
-
-        try {
-            System.out.println("The server has launched!");
-            ServerSocket server = new ServerSocket(10027);
-            while(true) {
-                n = incoming.size();
-                incoming.add(server.accept());
-                System.out.println("Accept client No." + n);
-
-                isr.add(new InputStreamReader(incoming.get(n).getInputStream()));
-                in.add(new BufferedReader(isr.get(n)));
-                out.add(new PrintWriter(incoming.get(n).getOutputStream(), true));
-
-                myClientProcThread.add(
-                        new ClientProcThread(n, incoming.get(n), isr.get(n), in.get(n), out.get(n)));
-                myClientProcThread.get(n).start(); // start thread
-
-		if(myClientProcThread.size() == 2){
-		    numBall = myBallMoveThread.size();
-		    myBallMoveThread.add(new BallMoveThread(numBall, 400, 400));
-		    myBallMoveThread.get(numBall).start();
-		}
-            }
-        } catch (Exception e) {
-            System.out.println("Error occured when socket was being created: " + e );
-        }
-    }
+  }
+  catch (Exception e) {
+    System.out.println("Error occured when socket was being created: " + e);
+  }
+}
 }
