72f0de47dd8161a2073fc87f9c6e510842391aa2
supaman4561 <dosu416@gmail.com>
2017-12-13 23:29:09 +0900

    generate block (correct)

--- src/blockbreak/GameMainController.java
+++ src/blockbreak/GameMainController.java
@@ -8,6 +8,7 @@ package blockbreak;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import javafx.application.*;
 import javafx.fxml.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
@@ -47,11 +48,10 @@ public class GameMainController implements Initializable {
      * for sending to Server
      */
     private static Socket mainSocket = null;
-    private int id;
+    private static int id;
     private ArrayList<Circle> arrayBall = new ArrayList<Circle>();
     private int ballMax = 1;
-    private ArrayList<Rectangle> myblock = new ArrayList<Rectangle>();
-    private ArrayList<Rectangle> enemyblock = new ArrayList<Rectangle>();
+    private ArrayList<ColoredRect> arrayBlock = new ArrayList<ColoredRect>();
     private Rectangle EnemyPaddle = new Rectangle(120,130,60,5);
     private Rectangle MyPaddle = new Rectangle(120,487,60,5);
 
@@ -79,16 +79,11 @@ public class GameMainController implements Initializable {
 
         for (int i=0; i<ballMax; i++){
             arrayBall.add(new Circle(5.0));
-            arrayBall.get(i).visibleProperty().bind(new SimpleBooleanProperty(false));
-        }
-
-        for (int i=0; i<20; i++){
-            myblock.add(new Rectangle());
-            enemyblock.add(new Rectangle());
+            arrayBall.get(i).visibleProperty().bind(new SimpleBooleanProperty(true));
         }
 
         try {
-            mainSocket = new Socket("172.22.3.152", 10027);
+            mainSocket = new Socket("localhost", 10027);
         } catch (UnknownHostException e) {
             System.err.println("Not found IPAddress of host." + e);
         } catch (IOException e) {
@@ -122,7 +117,7 @@ public class GameMainController implements Initializable {
 	}
 	return mySpeed;
     }
-    
+
     public class MesgRecvThread extends Thread {
 
         Socket socket;
@@ -145,171 +140,64 @@ public class GameMainController implements Initializable {
                 while(true) {
                     String inputLine = br.readLine();
                     if(inputLine != null) {
-			// for debug
-                        //System.out.println(inputLine);
+			             // for debug
+                        // System.out.println(inputLine);
                         String[] inputTokens = inputLine.split(",");
                         String cmd = inputTokens[0];
-			if(cmd.equals("Hello")){
-			    id = Integer.parseInt(inputTokens[1]);
-			}else if(cmd.equals("Ball")){
-			    int ballId = Integer.parseInt(inputTokens[1]);
-			    
+                        if(cmd.equals("Hello")){
+                            System.out.println(inputLine);
+                            id = Integer.parseInt(inputTokens[1]);
+                        }else if(cmd.equals("Ball")){
+                            int ballId = Integer.parseInt(inputTokens[1]);
+
                             Thread thread = new BallMoveThread(arrayBall.get(ballId),
-							       Integer.parseInt(inputTokens[2]),
-							       Integer.parseInt(inputTokens[3]));
-			    thread.start();
-			}else if(cmd.equals("Paddle")) {
-			    if (inputTokens[1].equals("LEFT")) mySpeed = MoveLeft(MyPaddle, mySpeed);
-			    if (inputTokens[1].equals("RIGHT")) mySpeed =  MoveRight(MyPaddle, mySpeed);
-			    String SendMesg = new String("Paddle," + id + "," + MyPaddle.getX());
-			    myOut.println(SendMesg);
+                                                               Integer.parseInt(inputTokens[2]),
+                                                               Integer.parseInt(inputTokens[3]));
+                            thread.start();
+                        }else if(cmd.equals("Paddle")) {
+                            if (inputTokens[1].equals("LEFT")) mySpeed = MoveLeft(MyPaddle, mySpeed);
+                            if (inputTokens[1].equals("RIGHT")) mySpeed =  MoveRight(MyPaddle, mySpeed);
+                            String SendMesg = new String("Paddle," + id + "," + MyPaddle.getX());
+                            myOut.println(SendMesg);
 
-			}else if (cmd.equals("EnemyPaddle")) {
-			    EnemyPaddle.setX(-1.0*Float.valueOf(inputTokens[2]));
+                        }else if (cmd.equals("EnemyPaddle")) {
+                            EnemyPaddle.setX(-1.0*Float.valueOf(inputTokens[2]));
                         }else if(cmd.equals("Blockset")){
-			    Rectangle target;
-			    int x = Integer.parseInt(inputTokens[1]);
-			    int y = Integer.parseInt(inputTokens[2]);
-			    int b = Integer.parseInt(inputTokens[3]);
+                            ColoredRect target;
+                            int x;
+                            int y;
 
+                            if(inputTokens[1].equals("end")){
+                                if(id % 2 == 1){
+                                     Collections.reverse(arrayBlock);
+                                }
 
-			    if(b<20){
-				target = enemyblock.get(b);
-				target.setX(x);
-				target.setY(y);
-				target.setWidth(50);
-				target.setHeight(20);
-				switch(b%5){
-				case 0:
-				    target.setFill(Color.RED);
-				    break;
-				case 1:
-				    target.setFill(Color.BLUE);
-				    break;
-				case 2:
-				    target.setFill(Color.YELLOW);
-				    break;
-				case 3:
-				    target.setFill(Color.GREEN);
-				    break;
-				case 4:
-				    target.setFill(Color.ORANGE);
-				    break;
-				}
-				target.setStroke(Color.BLACK);
-				target.setStrokeWidth(1);
-			    }else{
-				target = myblock.get(b-20);
-				target.setX(x);
-				target.setY(y);
-				target.setWidth(50);
-				target.setHeight(20);
-				switch(b%5){
-				case 0:
-				    target.setFill(Color.RED);
-				    break;
-				case 1:
-				    target.setFill(Color.BLUE);
-				    break;
-				case 2:
-				    target.setFill(Color.YELLOW);
-				    break;
-				case 3:
-				    target.setFill(Color.GREEN);
-				    break;
-				case 4:
-				    target.setFill(Color.ORANGE);
-				    break;
-				}
-				target.setStroke(Color.BLACK);
-				target.setStrokeWidth(1);
-			    }
+                                System.out.println(id);
+                                for(int i=0; i < arrayBlock.size() ; i++){
+                                    System.out.println(arrayBlock.get(i).getY());
+                                }
+                                Platform.runLater(() -> root.getChildren().addAll(arrayBlock));
+                            }else{
+                                x = Integer.parseInt(inputTokens[1]);
+                                y = Integer.parseInt(inputTokens[2]);
 
-			    if(id%2 == 1){
-				Collections.reverse(myblock);
-				Collections.reverse(enemyblock);
-			    }
-			    
-			    /*if((b<20&&id%2==0)||(b>=20&&id%2==1)){
-				if(b>=20){
-				    b-=20;
-				}
-				if(id%2==1){
-				    x = 300-x-50;
-				    y = 600-y-20;
-				}
-				
-				enemyblock.get(b).setX(x);
-				enemyblock.get(b).setY(y);
-				enemyblock.get(b).setWidth(50);
-				enemyblock.get(b).setHeight(20);
-				switch(b%5){
-				case 0:
-				    enemyblock.get(b).setFill(Color.RED);
-				    break;
-				case 1:
-				    enemyblock.get(b).setFill(Color.BLUE);
-				    break;
-				case 2:
-				    enemyblock.get(b).setFill(Color.YELLOW);
-				    break;
-				case 3:
-				    enemyblock.get(b).setFill(Color.GREEN);
-				    break;
-				case 4:
-				    enemyblock.get(b).setFill(Color.ORANGE);
-				    break;
-				}
-				enemyblock.get(b).setStroke(Color.BLACK);
-				enemyblock.get(b).setStrokeWidth(1);
-			    }else{
-				if(b>=20){
-				    b-=20;
-				}
-				if(id%2==1){
-				    x = 300-x-50;
-				    y = 600-y-20;
-				}
-				
-				myblock.get(b).setX(x);
-				myblock.get(b).setY(y);
-				myblock.get(b).setWidth(50);
-				myblock.get(b).setHeight(20);
-				switch(b%5){
-				case 0:
-				    myblock.get(b).setFill(Color.RED);
-				    break;
-				case 1:
-				    myblock.get(b).setFill(Color.BLUE);
-				    break;
-				case 2:
-				    myblock.get(b).setFill(Color.YELLOW);
-				    break;
-				case 3:
-				    myblock.get(b).setFill(Color.GREEN);
-				    break;
-				case 4:
-				    myblock.get(b).setFill(Color.ORANGE);
-				    break;
-				}
-				myblock.get(b).setStroke(Color.BLACK);
-				myblock.get(b).setStrokeWidth(1);
-				}*/
+                                // width : 50 , height : 20
+                                target = new ColoredRect(x, y, 50, 20);
+                                target.setColor(arrayBlock.size());
+                                arrayBlock.add(target);
 
-			}else if(cmd.equals("BlockDelete")){
-			    int b = Integer.parseInt(inputTokens[1]);
-			    Thread thread = new BlockDeleteThread(b);
-			    thread.start();
-			}
+                            }
 
-
-
-                    }else{
-                        break;
+                        }else if(cmd.equals("BlockDelete")){
+                            int blockId = Integer.parseInt(inputTokens[1]);
+                            arrayBlock.get(blockId).visibleProperty().bind(new SimpleBooleanProperty(false));
+                        }else{
+                            break;
+                        }
                     }
-		    mySpeed = MovePaddle(MyPaddle, mySpeed);
+		            mySpeed = MovePaddle(MyPaddle, mySpeed);
                 }
-		socket.close();
+		        socket.close();
             } catch (IOException e) {
                 System.err.println("error occured: " + e);
             }
@@ -339,35 +227,6 @@ public class GameMainController implements Initializable {
         }
     }
 
-    class BlockDeleteThread extends Thread{
-
-	int b;
-
-	BlockDeleteThread(int b){
-	    this.b = b;
-	}
-
-	public void run(){
-	     if((b<20&&id%2==0)||(b>=20&&id%2==1)){
-		 if(b>=20){
-		     b-=20;
-		 }
-		 //root.getChildren().remove(enemyblock.get(b));
-		 enemyblock.get(b).visibleProperty().bind(new SimpleBooleanProperty(false));
-		 //enemyblock.get(b).setStroke(Color.WHITE);
-	     }else{
-		 if(b>=20){
-		     b-=20;
-		 }
-		 //root.getChildren().remove(myblock.get(b));
-		 myblock.get(b).visibleProperty().bind(new SimpleBooleanProperty(false));
-		 //myblock.get(b).setFill(Color.WHITE);
-		 //myblock.get(b).setStroke(Color.WHITE);
-
-	     }
-	}
-    }
-
 
     /**
      * return instance(singleton)
@@ -380,7 +239,7 @@ public class GameMainController implements Initializable {
     public void show() {
         BlockBreak.getPresentStage().setScene(SCENE);
     }
-    
+
     @FXML
     private void handleKeyPressed(KeyEvent event) {
 	//  System.out.println("keypressed");
@@ -388,7 +247,7 @@ public class GameMainController implements Initializable {
 	myOut.println(event.getCode());
 	myOut.flush();
     }
-  
+
     /**
      * Initializes the controller class.
      */
@@ -398,8 +257,6 @@ public class GameMainController implements Initializable {
         myName.setText(BlockBreak.getUserName());
 
 	root.getChildren().addAll(arrayBall);
-	root.getChildren().addAll(myblock);
-	root.getChildren().addAll(enemyblock);
 	root.getChildren().add(EnemyPaddle);
 	root.getChildren().add(MyPaddle);
 
